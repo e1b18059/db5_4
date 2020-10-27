6e3d90119798dd70b217b827e2ce08c6c05ee43d
e1b18059 <e1b18059@oit.ac.jp>
2020-10-27 15:20:14 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/group5_4/db5_4/Db54Application.java
@@ -0,0 +1,13 @@
+package oit.is.group5_4.db5_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db54Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db54Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group5_4/db5_4/Db54ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group5_4.db5_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db54ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
